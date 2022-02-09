9c175a6898552b733baa367dd363aa6350802f42
XbozzX <muhammad_19000649@utp.edu.my>
2022-02-09 15:20:49 +0800

    Set up the project

---
+++ src/com/learnProgramming/single_room.java
@@ -0,0 +1,14 @@
+package com.learnProgramming;
+
+public class single_room {
+
+    private dimension wall;
+    private boolean marble;
+    private boolean window;
+    private furniture chair;
+
+    public single_room(dimension wall, boolean marble, boolean window, furniture chair){
+        this.wall = wall;
+        this.
+    }
+}
